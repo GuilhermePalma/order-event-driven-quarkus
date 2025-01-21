c4fc8bdfa79ddb17bd96e0a65a8bd866f00d8d9a
Guilherme Palma <guippalma@gmail.com>
2025-01-21 19:24:27 -0300

    feat: add api description

--- order-spring-boot/src/main/java/com/guilhermepalma/order_spring_boot/OrderSpringBootApplication.java
+++ order-spring-boot/src/main/java/com/guilhermepalma/order_spring_boot/OrderSpringBootApplication.java
@@ -3,9 +3,17 @@ package com.guilhermepalma.order_spring_boot;
 import com.guilhermepalma.order_spring_boot.factory.OperationSQLFactory;
 import com.guilhermepalma.order_spring_boot.factory.Operations;
 import com.guilhermepalma.order_spring_boot.factory.OperationsFactory;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+		title = "API Spring Boot - Producer, Consumer, Query of Orders",
+		version = "0.0.1",
+		description = "API usada para as Operações Server-Side, usando EventDriven e segregando a Logica do Kafka do Banco de Dados"
+))
 @SpringBootApplication
 public class OrderSpringBootApplication {
 
