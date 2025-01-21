8cddb363fb9d9677526f25b1517c062a97fb268d
Guilherme Palma <guippalma@gmail.com>
2025-01-20 22:43:56 -0300

    feat(#5) create initial spring project

---
+++ order-spring-boot/src/main/java/com/guilhermepalma/order_spring_boot/OrderSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermepalma.order_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSpringBootApplication.class, args);
+	}
+
+}
---
+++ order-spring-boot/src/test/java/com/guilhermepalma/order_spring_boot/OrderSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermepalma.order_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
