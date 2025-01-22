b017abe329a7a6f5ad5b532dd9c11ea2af3d3047
Guilherme Palma <guippalma@gmail.com>
2025-01-21 23:18:31 -0300

    fix(#14): adjust properties

--- order-spring-boot/src/main/java/com/guilhermepalma/order_spring_boot/model/Order.java
+++ order-spring-boot/src/main/java/com/guilhermepalma/order_spring_boot/model/Order.java
@@ -3,10 +3,7 @@ package com.guilhermepalma.order_spring_boot.model;
 import com.guilhermepalma.order_spring_boot.type.StatusType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -15,11 +12,12 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity(name = "TB_ORDER")
+@ToString
 public class Order {
 
     // Default class values
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(unique = true, nullable = false, columnDefinition = "uuid")
     private UUID id;
 
@@ -28,15 +26,15 @@ public class Order {
     private String customerName;
 
     @Schema(description = "Name of Product", example = "DESKTOP I3 - 918391")
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String product;
 
     @Schema(description = "Quantity of Product", example = "3")
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private int quantity;
 
     @Schema(description = "Status of Product", example = "PENDING")
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private StatusType status;
 
     @Schema(description = "defines if the register is deleted. this tag allows you to retrieve possible excluded cases", defaultValue = "false")
